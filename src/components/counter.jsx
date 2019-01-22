56d03356cc2ae865672d47e51f3775a23194170f
Vladimir Hidalgo <Vladimir Hidalgo>
2019-01-22 15:52:16 -0600

    Stage 8

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={`badge m-2 ${this.getBadgeClasses()}`}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  getBadgeClasses() {
+    const { count } = this.state;
+    return count === 0 ? "badge-warning" : "badge-primary";
   }
 }
 export default Counter;
