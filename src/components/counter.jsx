8950c89495f9fcd056fe29db9ad847855f7a13ae
Vladimir Hidalgo <Vladimir Hidalgo>
2019-01-22 16:06:23 -0600

    Stage 9

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,34 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: ["taggy", "truthy"]
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
-        <span className={`badge m-2 ${this.getBadgeClasses()}`}>
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
-  getBadgeClasses() {
-    const { count } = this.state;
-    return count === 0 ? "badge-warning" : "badge-primary";
   }
 }
 export default Counter;
